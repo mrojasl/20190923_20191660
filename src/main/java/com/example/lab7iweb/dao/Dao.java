d635579b551749751b0054866a5267c7351d7cb1
Jorge Guillen Rivera <jorgegrr052002@gmail.com>
2022-06-06 19:48:14 -0500

    a

--- src/main/java/com/example/lab7iweb/dao/Dao.java
+++ src/main/java/com/example/lab7iweb/dao/Dao.java
@@ -1,4 +1,38 @@
 package com.example.lab7iweb.dao;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Dao {
+    public ArrayList<Bean> listarTrabajos(){
+        ArrayList<Bean> listaTrabajos = new ArrayList<>();
+
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/hr";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("select * from jobs");) {
+
+            while(rs.next()){
+                BJob bJob = new BJob();
+                bJob.setJobId(rs.getString(1));
+                bJob.setJobTitle(rs.getString(2));
+                bJob.setMinSalary(rs.getInt(3));
+                bJob.setMaxSalary(rs.getInt(4));
+                listaTrabajos.add(bJob);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return listaTrabajos;
+    }
 }
